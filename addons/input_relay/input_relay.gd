04a6716949491e773bd36adcb1d221810b0369a4
Joaquin Kiko <joaquinkiko@proton.me>
2026-09-02 15:07:51 -0600

    feat: add device connection

--- addons/input_relay/input_relay.gd
+++ addons/input_relay/input_relay.gd
@@ -7,3 +7,31 @@ var devices: Array[InputRelayDevice]
 var players: Array[InputRelayPlayer]
 ## All [InputActionSet]s sets, sorted by name
 var action_sets: Dictionary[StringName, InputActionSet]
+
+func _ready() -> void:
+	# Setup device connections
+	Input.joy_connection_changed.connect(_joy_connection_changed)
+	for id in Input.get_connected_joypads():
+		_register_device(id)
+
+func _joy_connection_changed(device_id: int, connected: bool) -> void:
+	if connected:
+		_register_device(device_id)
+	else:
+		_unregister_device(device_id)
+
+func _register_device(device_id: int) -> void:
+	var device := InputRelayDevice.new(device_id, Input.get_joy_name(device_id))
+	devices.append(device)
+
+func _unregister_device(device_id: int) -> void:
+	# Remove from player assignments
+	for player in players:
+		for device in player.devices.duplicate():
+			if device.index == device_id:
+				player.devices.erase(device)
+	# Remove from devices
+	for device in devices:
+		if device.index == device_id:
+			devices.erase(device_id)
+			break
--- addons/input_relay/internal/device.gd
+++ addons/input_relay/internal/device.gd
@@ -2,8 +2,9 @@
 class_name InputRelayDevice extends RefCounted
 
 enum Features {
-	LIGHTS,
-	GYRO,
+	LIGHTS = 1 << 0,
+	MOTION = 1 << 1,
+	HAPTIC = 1 << 2,
 }
 
 ## Internal index of device
@@ -14,3 +15,26 @@ var name: String
 var feature_flags: int
 ## [DeviceGlyphMap] to use
 var glyph_map: DeviceGlyphMap
+
+func _init(device_id: int, device_name: String) -> void:
+	self.index = device_id
+	self.name = device_name
+	# Assign feature flags
+	if Input.get_connected_joypads().has(device_id):
+		if Input.has_joy_light(device_id):
+			feature_flags |= Features.LIGHTS
+		if Input.has_joy_motion_sensors(device_id):
+			feature_flags |= Features.MOTION
+		if Input.has_joy_vibration(device_id):
+			feature_flags |= Features.HAPTIC
+	# Detect type
+	# TODO: Finish glyph_map assignment | Improve detection
+	var lname := device_name.to_lower()
+	if "xbox" in lname || "xinput" in lname:
+		pass
+	elif "playstation" in lname || "dualshock" in lname || "dualsense" in lname:
+		pass
+	elif "nintendo" in lname || "switch" in lname:
+		pass
+	else:
+		pass
