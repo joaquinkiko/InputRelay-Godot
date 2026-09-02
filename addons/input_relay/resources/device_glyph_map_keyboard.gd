aee9896fa239fc3fd220225a87e7f475f368752d
Joaquin Kiko <joaquinkiko@proton.me>
2026-09-02 14:41:39 -0600

    feat: add keyboard glyph map

---
+++ addons/input_relay/resources/device_glyph_map_keyboard.gd
@@ -0,0 +1,223 @@
+## [DeviceGlyphMap] for mouse and keyboard
+class_name DeviceGlyphMapKeyboard extends Resource
+
+@export_group("Mouse")
+@export var mouse_left_glyph: Texture2D
+@export var mouse_left_string: StringName = "Left Click"
+@export var mouse_right_glyph: Texture2D
+@export var mouse_right_string: StringName = "Right Click"
+@export var mouse_middle_glyph: Texture2D
+@export var mouse_middle_string: StringName = "Middle Click"
+@export var mouse_wheel_up_glyph: Texture2D
+@export var mouse_wheel_up_string: StringName = "Scroll Up"
+@export var mouse_wheel_down_glyph: Texture2D
+@export var mouse_wheel_down_string: StringName = "Scroll Down"
+@export var mouse_wheel_left_glyph: Texture2D
+@export var mouse_wheel_left_string: StringName = "Scroll Left"
+@export var mouse_wheel_right_glyph: Texture2D
+@export var mouse_wheel_right_string: StringName = "Scroll Right"
+@export var mouse_extra1_glyph: Texture2D
+@export var mouse_extra1_string: StringName = "Mouse Button 4"
+@export var mouse_extra2_glyph: Texture2D
+@export var mouse_extra2_string: StringName = "Mouse Button 5"
+
+@export_group("Letters")
+@export var a_glyph: Texture2D
+@export var a_string: StringName = "A"
+@export var b_glyph: Texture2D
+@export var b_string: StringName = "B"
+@export var c_glyph: Texture2D
+@export var c_string: StringName = "C"
+@export var d_glyph: Texture2D
+@export var d_string: StringName = "D"
+@export var e_glyph: Texture2D
+@export var e_string: StringName = "E"
+@export var f_glyph: Texture2D
+@export var f_string: StringName = "F"
+@export var g_glyph: Texture2D
+@export var g_string: StringName = "G"
+@export var h_glyph: Texture2D
+@export var h_string: StringName = "H"
+@export var i_glyph: Texture2D
+@export var i_string: StringName = "I"
+@export var j_glyph: Texture2D
+@export var j_string: StringName = "J"
+@export var k_glyph: Texture2D
+@export var k_string: StringName = "K"
+@export var l_glyph: Texture2D
+@export var l_string: StringName = "L"
+@export var m_glyph: Texture2D
+@export var m_string: StringName = "M"
+@export var n_glyph: Texture2D
+@export var n_string: StringName = "N"
+@export var o_glyph: Texture2D
+@export var o_string: StringName = "O"
+@export var p_glyph: Texture2D
+@export var p_string: StringName = "P"
+@export var q_glyph: Texture2D
+@export var q_string: StringName = "Q"
+@export var r_glyph: Texture2D
+@export var r_string: StringName = "R"
+@export var s_glyph: Texture2D
+@export var s_string: StringName = "S"
+@export var t_glyph: Texture2D
+@export var t_string: StringName = "T"
+@export var u_glyph: Texture2D
+@export var u_string: StringName = "U"
+@export var v_glyph: Texture2D
+@export var v_string: StringName = "V"
+@export var w_glyph: Texture2D
+@export var w_string: StringName = "W"
+@export var x_glyph: Texture2D
+@export var x_string: StringName = "X"
+@export var y_glyph: Texture2D
+@export var y_string: StringName = "Y"
+@export var z_glyph: Texture2D
+@export var z_string: StringName = "Z"
+
+@export_group("Numbers")
+@export var key_0_glyph: Texture2D
+@export var key_0_string: StringName = "0"
+@export var key_1_glyph: Texture2D
+@export var key_1_string: StringName = "1"
+@export var key_2_glyph: Texture2D
+@export var key_2_string: StringName = "2"
+@export var key_3_glyph: Texture2D
+@export var key_3_string: StringName = "3"
+@export var key_4_glyph: Texture2D
+@export var key_4_string: StringName = "4"
+@export var key_5_glyph: Texture2D
+@export var key_5_string: StringName = "5"
+@export var key_6_glyph: Texture2D
+@export var key_6_string: StringName = "6"
+@export var key_7_glyph: Texture2D
+@export var key_7_string: StringName = "7"
+@export var key_8_glyph: Texture2D
+@export var key_8_string: StringName = "8"
+@export var key_9_glyph: Texture2D
+@export var key_9_string: StringName = "9"
+@export var kp_0_glyph: Texture2D
+
+@export_group("Symbols")
+@export var apostrophe_glyph: Texture2D
+@export var apostrophe_string: StringName = "'"
+@export var comma_glyph: Texture2D
+@export var comma_string: StringName = ","
+@export var minus_glyph: Texture2D
+@export var minus_string: StringName = "-"
+@export var period_glyph: Texture2D
+@export var period_string: StringName = "."
+@export var slash_glyph: Texture2D
+@export var slash_string: StringName = "/"
+@export var semicolon_glyph: Texture2D
+@export var semicolon_string: StringName = ";"
+@export var equal_glyph: Texture2D
+@export var equal_string: StringName = "="
+@export var bracketleft_glyph: Texture2D
+@export var bracketleft_string: StringName = "["
+@export var backslash_glyph: Texture2D
+@export var backslash_string: StringName = "\\"
+@export var bracketright_glyph: Texture2D
+@export var bracketright_string: StringName = "]"
+@export var quoteleft_glyph: Texture2D
+@export var quoteleft_string: StringName = "`"
+
+
+@export_group("Modifiers")
+@export var shift_glyph: Texture2D
+@export var shift_string: StringName = "Shift"
+@export var ctrl_glyph: Texture2D
+@export var ctrl_string: StringName = "Ctrl"
+@export var alt_glyph: Texture2D
+@export var alt_string: StringName = "Alt"
+@export var capslock_glyph: Texture2D
+@export var capslock_string: StringName = "Caps Lock"
+
+@export_group("Directional")
+@export var left_glyph: Texture2D
+@export var left_string: StringName = "Left Arrow"
+@export var up_glyph: Texture2D
+@export var up_string: StringName = "Up Arrow"
+@export var right_glyph: Texture2D
+@export var right_string: StringName = "Right Arrow"
+@export var down_glyph: Texture2D
+@export var down_string: StringName = "Down Arrow"
+@export var pageup_glyph: Texture2D
+@export var pageup_string: StringName = "Page Up"
+@export var pagedown_glyph: Texture2D
+@export var pagedown_string: StringName = "Page Down"
+
+@export_group("Numpad")
+@export var kp_0_string: StringName = "Numpad 0"
+@export var kp_1_glyph: Texture2D
+@export var kp_1_string: StringName = "Numpad 1"
+@export var kp_2_glyph: Texture2D
+@export var kp_2_string: StringName = "Numpad 2"
+@export var kp_3_glyph: Texture2D
+@export var kp_3_string: StringName = "Numpad 3"
+@export var kp_4_glyph: Texture2D
+@export var kp_4_string: StringName = "Numpad 4"
+@export var kp_5_glyph: Texture2D
+@export var kp_5_string: StringName = "Numpad 5"
+@export var kp_6_glyph: Texture2D
+@export var kp_6_string: StringName = "Numpad 6"
+@export var kp_7_glyph: Texture2D
+@export var kp_7_string: StringName = "Numpad 7"
+@export var kp_8_glyph: Texture2D
+@export var kp_8_string: StringName = "Numpad 8"
+@export var kp_9_glyph: Texture2D
+@export var kp_9_string: StringName = "Numpad 9"
+@export var kp_multiply_glyph: Texture2D
+@export var kp_multiply_string: StringName = "Numpad *"
+@export var kp_divide_glyph: Texture2D
+@export var kp_divide_string: StringName = "Numpad /"
+@export var kp_subtract_glyph: Texture2D
+@export var kp_subtract_string: StringName = "Numpad -"
+@export var kp_period_glyph: Texture2D
+@export var kp_period_string: StringName = "Numpad ."
+@export var kp_add_glyph: Texture2D
+@export var kp_add_string: StringName = "Numpad +"
+@export var kp_enter_glyph: Texture2D
+@export var kp_enter_string: StringName = "Numpad Enter"
+
+@export_group("Function")
+@export var f1_glyph: Texture2D
+@export var f1_string: StringName = "F1"
+@export var f2_glyph: Texture2D
+@export var f2_string: StringName = "F2"
+@export var f3_glyph: Texture2D
+@export var f3_string: StringName = "F3"
+@export var f4_glyph: Texture2D
+@export var f4_string: StringName = "F4"
+@export var f5_glyph: Texture2D
+@export var f5_string: StringName = "F5"
+@export var f6_glyph: Texture2D
+@export var f6_string: StringName = "F6"
+@export var f7_glyph: Texture2D
+@export var f7_string: StringName = "F7"
+@export var f8_glyph: Texture2D
+@export var f8_string: StringName = "F8"
+@export var f9_glyph: Texture2D
+@export var f9_string: StringName = "F9"
+@export var f10_glyph: Texture2D
+@export var f10_string: StringName = "F10"
+@export var f11_glyph: Texture2D
+@export var f11_string: StringName = "F11"
+@export var f12_glyph: Texture2D
+@export var f12_string: StringName = "F12"
+
+@export_group("Misc")
+@export var escape_glyph: Texture2D
+@export var escape_string: StringName = "Escape"
+@export var tab_glyph: Texture2D
+@export var tab_string: StringName = "Tab"
+@export var backspace_glyph: Texture2D
+@export var backspace_string: StringName = "Backspace"
+@export var enter_glyph: Texture2D
+@export var enter_string: StringName = "Enter"
+@export var insert_glyph: Texture2D
+@export var insert_string: StringName = "Insert"
+@export var delete_glyph: Texture2D
+@export var delete_string: StringName = "Delete"
+@export var space_glyph: Texture2D
+@export var space_string: StringName = "Space"
