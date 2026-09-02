624c665cc1a48d1d1cb60351c3d91dfa2064b78b
Joaquin Kiko <joaquinkiko@proton.me>
2026-09-02 11:04:11 -0600

    feat: add gamepad glyph class

---
+++ addons/input_relay/resources/device_glyph_map_gamepad.gd
@@ -0,0 +1,74 @@
+## [DeviceGlyphMap] for SDL gamepads
+class_name DeviceGlyphMapGamepad extends Resource
+
+@export_group("Face Buttons")
+@export var south_glyph: Texture2D
+@export var south_string: StringName = "South Button"
+@export var east_glyph: Texture2D
+@export var east_string: StringName = "East Button"
+@export var west_glyph: Texture2D
+@export var west_string: StringName = "West Button"
+@export var north_glyph: Texture2D
+@export var north_string: StringName = "North Button"
+
+@export_group("Menu Buttons")
+@export var back_glyph: Texture2D
+@export var back_string: StringName = "Back"
+@export var guide_glyph: Texture2D
+@export var guide_string: StringName = "Guide"
+@export var start_glyph: Texture2D
+@export var start_string: StringName = "Start"
+
+@export_group("D-Pad")
+@export var dpad_up_glyph: Texture2D
+@export var dpad_up_string: StringName = "D-Pad Up"
+@export var dpad_down_glyph: Texture2D
+@export var dpad_down_string: StringName = "D-Pad Down"
+@export var dpad_left_glyph: Texture2D
+@export var dpad_left_string: StringName = "D-Pad Left"
+@export var dpad_right_glyph: Texture2D
+@export var dpad_right_string: StringName = "D-Pad Right"
+
+@export_group("Sticks")
+@export var left_stick_glyph: Texture2D
+@export var left_stick_string: StringName = "Left Stick"
+@export var right_stick_glyph: Texture2D
+@export var right_stick_string: StringName = "Right Stick"
+@export var left_stick_press_glyph: Texture2D
+@export var left_stick_press_string: StringName = "Left Stick Click"
+@export var right_stick_press_glyph: Texture2D
+@export var right_stick_press_string: StringName = "Right Stick Click"
+
+@export_group("Shoulders / Triggers")
+@export var left_shoulder_glyph: Texture2D
+@export var left_shoulder_string: StringName = "Left Bumper"
+@export var right_shoulder_glyph: Texture2D
+@export var right_shoulder_string: StringName = "Right Bumper"
+@export var left_trigger_glyph: Texture2D
+@export var left_trigger_string: StringName = "Left Trigger"
+@export var right_trigger_glyph: Texture2D
+@export var right_trigger_string: StringName = "Right Trigger"
+
+@export_group("Misc")
+@export var misc1_glyph: Texture2D
+@export var misc1_string: StringName = "Share"
+@export var misc2_glyph: Texture2D
+@export var misc2_string: StringName = "Misc Button 2"
+@export var misc3_glyph: Texture2D
+@export var misc3_string: StringName = "Misc Button 3"
+@export var misc4_glyph: Texture2D
+@export var misc4_string: StringName = "Misc Button 4"
+@export var misc5_glyph: Texture2D
+@export var misc5_string: StringName = "Misc Button 5"
+@export var misc6_glyph: Texture2D
+@export var misc6_string: StringName = "Misc Button 6"
+@export var paddle1_glyph: Texture2D
+@export var paddle1_string: StringName = "Paddle 1"
+@export var paddle2_glyph: Texture2D
+@export var paddle2_string: StringName = "Paddle 2"
+@export var paddle3_glyph: Texture2D
+@export var paddle3_string: StringName = "Paddle 3"
+@export var paddle4_glyph: Texture2D
+@export var paddle4_string: StringName = "Paddle 4"
+@export var touchpad_glyph: Texture2D
+@export var touchpad_string: StringName = "Touchpad"
