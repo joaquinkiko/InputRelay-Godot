d7b0b2e0bc99861aa1803424dd0806416329c0c8
Joaquin Kiko <joaquinkiko@proton.me>
2026-09-06 14:38:20 -0600

    chore: remove unused device name field on DeviceGlyphMap

--- addons/input_relay/resources/device_glyph_map.gd
+++ addons/input_relay/resources/device_glyph_map.gd
@@ -4,7 +4,5 @@ class_name DeviceGlyphMap extends Resource
 
 ## Device glyph
 @export var device_glyph: Texture2D
-## Human-readable device name
-@export var device_string: StringName
 ## Used when no valid glyph can be found
 @export var fallback_glyph: Texture2D
